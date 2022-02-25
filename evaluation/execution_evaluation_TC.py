cea417f8c6617e0aed841b3f800b10d7c254385a
mahimanzum <mahimanzum@gmail.com>
2022-02-25 03:40:39 +0000

    thread safe functions written

--- evaluation/execution_evaluation_TC.py
+++ evaluation/execution_evaluation_TC.py
@@ -61,31 +61,30 @@ def compare_files(file1, file2):
     except Exception as e:
         print("exception = ", e)
         return False
-def run_python(code, test_case_folder):
-    with open('garbage/Main.py', 'w', encoding='utf8') as fw:
+def run_python(code, test_case_folder, idx):
+    with open(f'garbage/{idx}_Main.py', 'w', encoding='utf8') as fw:
         fw.write(code)
     in_files = glob(test_case_folder+"/in/*")
-    p1 = subprocess.run(["python","-m", "py_compile", "garbage/Main.py"], stderr=PIPE)
+    p1 = subprocess.run(["python","-m", "py_compile", f"garbage/{idx}_Main.py"], stderr=PIPE)
     return_code = p1.returncode
     python2 = False
     if (return_code):
-        p1 = subprocess.run(["python2","-m", "py_compile", "garbage/Main.py"], stderr=PIPE)
+        p1 = subprocess.run(["python2","-m", "py_compile", f"garbage/{idx}_Main.py"], stderr=PIPE)
         return_code = p1.returncode
         python2=True
 
     if(return_code):
         print("doesnt compile", return_code)
-        print(code)
-        
-        sys.exit(0)
+        #print(code)
+        #sys.exit(0)
         return False, 0, len(in_files)
 
     did_not_match = 0
     for in_file in in_files:
         stripped_TC = open(in_file).read().strip()
-        with open('garbage/stripped_TC.txt', 'w') as f:
+        with open(f'garbage/{idx}_stripped_TC.txt', 'w') as f:
             f.write(stripped_TC)
-        cmd = "python garbage/Main.py < {} > garbage/cmd_out.txt".format('garbage/stripped_TC.txt')
+        cmd = f"python garbage/{idx}_Main.py < garbage/{idx}_stripped_TC.txt > garbage/{idx}_cmd_out.txt"
         if (python2):
             cmd = cmd.replace("python", "python2")
         p = Popen(cmd, shell=True, stdin=PIPE, stdout=PIPE, stderr=subprocess.DEVNULL, close_fds=True)
@@ -95,26 +94,26 @@ def run_python(code, test_case_folder):
         out_file ="/".join(out)
         out_file = out_file.replace(".in", ".out")
 
-        p2 = subprocess.Popen(["cp",out_file, "garbage/cmd_out_match.txt"])
+        p2 = subprocess.Popen(["cp",out_file, f"garbage/{idx}_cmd_out_match.txt"])
         p2.wait()
-        if not compare_files('garbage/cmd_out.txt', 'garbage/cmd_out_match.txt'):
+        if not compare_files(f'garbage/{idx}_cmd_out.txt', f'garbage/{idx}_cmd_out_match.txt'):
             did_not_match+=1
             print(in_file)
     return True, len(in_files)-did_not_match,len(in_files)
 
-def run_java(code, test_case_folder):
+def run_java(code, test_case_folder, idx):
 
-    with open('garbage/Main.java', 'w', encoding='utf8') as fw:
+    with open(f'garbage/{idx}_Main.java', 'w', encoding='utf8') as fw:
         fw.write(code)
     in_files = glob(test_case_folder+"/in/*")
-    p1 = subprocess.run(["javac","garbage/Main.java"], stderr=PIPE)
+    p1 = subprocess.run(["javac",f"garbage/{idx}_Main.java"], stderr=PIPE)
     return_code = p1.returncode
     if(return_code):
         return False, 0, len(in_files)
 
     did_not_match = 0
     for in_file in in_files:
-        cmd = "java garbage/Main < {} > garbage/cmd_out.txt".format(in_file)
+        cmd = f"java garbage/{idx}_Main < {in_file} > garbage/{idx}_cmd_out.txt"
         p = Popen(cmd, shell=True, stdin=PIPE, stdout=PIPE, stderr=subprocess.DEVNULL, close_fds=True)
         p.wait()
         out = in_file.split("/")
@@ -122,16 +121,20 @@ def run_java(code, test_case_folder):
         out_file ="/".join(out)
         out_file = out_file.replace(".in", ".out")
 
-        p2 = subprocess.Popen(["cp",out_file, "garbage/cmd_out_match.txt"])
+        p2 = subprocess.Popen(["cp",out_file, f"garbage/{idx}_cmd_out_match.txt"])
         p2.wait()
-        if not compare_files('garbage/cmd_out.txt', 'garbage/cmd_out_match.txt'):
-
+        if not compare_files(f'garbage/{idx}_cmd_out.txt', f'garbage/{idx}_cmd_out_match.txt'):
             did_not_match+=1
+    
     return True, len(in_files)-did_not_match,len(in_files)
 
 def main(args):
 
     data = getJsonData(args.input)
+    output_programs = []
+    with open(args.model_output, encoding='utf8') as f:
+        for line in f:
+            output_programs.append(line.strip())
 
     root_folder = "../third_party"
     jprocessor = JavaProcessor(root_folder=root_folder)
@@ -178,45 +181,52 @@ def main(args):
     
     print("len(problemid_to_tc) = ", len(problemid_to_tc))
     
-    ran, total= 0, 0
+    ran_prev,ran_now, total= 0, 0,0
     uniq = set()
     invalid_problems = ['p02833','p02764','p03619','p03429', 'p03334','p03110', 'p03836', 'p03394', 'p02678', 'p03046', 'p04035', 'p02669', 'p02977', 'p02997', 'p03938', 'p02692', 'p03267', 'p02975', 'p02825', 'p03952', 'p02731', 'p02936', 'p02902', 'p03263', 'p02972', 'p02690', 'p04007', 'p03257', 'p03095', 'p03746', 'p02903', 'p03097', 'p02963', 'p03245', 'p02976', 'p02694', 'p02697', 'p03044', 'p02861', 'p02850']
-    for dt in tqdm(data[11378:]): #9630
+    
+    def execute_and_evaluate(idx, dt):
         if dt['tgt_id'].split("_")[0] in problemid_to_tc.keys():
             if dt['tgt_id'].split("_")[0] in invalid_problems:
                 print(" an invalid problem is in test which should not happen")
                 return
                 continue
             
-            code = processor.detokenize_code(dt['tgt'])
             if dt['tgt_id'] in uniq:
                 continue
             uniq.add(dt['tgt_id'])
             
             test_case_folder = problemid_to_tc[dt['tgt_id'].split("_")[0]]
             if args.lang=='java':
-                compiles, correctTC, totalTC = run_java(code,test_case_folder)
-                if(correctTC != totalTC):
-                    print("missed a test case", dt['tgt_id'])
-                ran+=correctTC
-                total+=totalTC
+                compiles, correctTC, totalTC = run_java(processor.detokenize_code(dt['tgt']),test_case_folder, idx)
+                if(compiles and correctTC == totalTC):
+                    compiles, correctTC, totalTC = run_java(processor.detokenize_code(dt['src']),test_case_folder, idx)
+                    ran_prev +=correctTC
+                    compiles, correctTC, totalTC = run_java(output_programs[idx],test_case_folder, idx)
+                    ran_now +=correctTC
+                    total+=totalTC
+
             if args.lang=='py':
-                compiles, correctTC, totalTC = run_python(code,test_case_folder)
-                if(correctTC != totalTC):
-                    print(correctTC, totalTC)
-                    print("missed a test case", dt['tgt_id'])
-                ran+=correctTC
-                total+=totalTC
-            
+                compiles, correctTC, totalTC = run_python(processor.detokenize_code(dt['tgt']),test_case_folder, idx)
+                if(compiles and correctTC == totalTC):
+                    compiles, correctTC, totalTC = run_python(processor.detokenize_code(dt['src']),test_case_folder, idx)
+                    ran_prev +=correctTC
+                    compiles, correctTC, totalTC = run_python(output_programs[idx],test_case_folder, idx)
+                    ran_now +=correctTC
+                    total+=totalTC
         else:
             print(dt['src_id'])
             print("a problem found for which we have no test case which should not happen")
             return
     
-    print("ran, total, success = ", ran, total, ran/total)
+    Parallel(n_jobs=12, prefer="threads")(delayed(execute_and_evaluate)(idx, dt) for idx, dt in tqdm(enumerate(data)))
+        
+    
+    print("ran_prev,ran_now, total, ran_prev/total, ran_now/total ", ran_prev,ran_now, total, ran_prev/total,ran_now/total )
             
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
+    parser.add_argument("--model_output", type=str, required=True, help="Path to sources")
     parser.add_argument("--input", type=str, required=True, help="Path to sources")
     parser.add_argument("--lang", type=str, required=True, help="Name of language")
     parser.add_argument("--test_cases", type=str, required=True, help="Name of language")
