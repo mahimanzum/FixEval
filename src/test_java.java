392cf62f4d31f323ea1862858ffc9a16d64edf84
mahimanzum <mahimanzum@gmail.com>
2022-03-01 15:38:26 +0000

    unnecessary stuffs removed

--- src/test_java.java
+++
@@ -1,9 +0,0 @@
-import java . util . Scanner ;
-class Main {
-  public static void main ( String [ ] args ) {
-    Scanner sc = new Scanner ( System . in ) ;
-    int n = sc . nextInt ( ) ;
-    int ans = ( n + 1 ) / 2 ;
-    System . out . println ( ans ) ;
-  }
-}
